dcb001ce910ed73580cef177e1ff3eb39ec1d84c
Francovg18 <153469619+Francovg18@users.noreply.github.com>
2024-05-19 23:18:32 -0400

    pregunta4.c

--- pregunta4.c
+++ pregunta4.c
@@ -1,11 +1,8 @@
-//4. Realizar en OPENMP la multiplicación de una matriz NxN, con un vector de dimensión N.
-//gcc -fopenmp pregunta4.c -o pregunta4
 #include <stdio.h>
 #include <omp.h>
 
 #define N 4
 
-// Función multiplicar matriz
 void matrizPorVector(int matriz[N][N], int vector[N], int resultado[N]) {
     int i,j;
     #pragma omp parallel for private(j)
@@ -17,10 +14,6 @@ void matrizPorVector(int matriz[N][N], int vector[N], int resultado[N]) {
 }
 
 void main() {
-    //int N;
-    //printf("Ingrese el valor de N");
-    //scanf("%d", &N);
-   
     int matriz[N][N];
     int vector[N];
     int resultado[N];
@@ -39,23 +32,3 @@ void main() {
     }
     printf("\n");
 }
-
-/*
-printf("Ingrese los elementos de la matriz (%d x %d):\n", N, N);
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            scanf("%d", &matriz[i][j]);
-        }
-    }
-
-printf("Ingrese los elementos del vector (%d elementos):\n", N);
-    for (int i = 0; i < N; i++) {
-        scanf("%d", &vector[i]);
-    }
-
-int matriz[N][N] = {{1, 2, 3, 4},
-                        {5, 6, 7, 8},
-                        {9, 10, 11, 12},
-                        {13, 14, 15, 16}};
-    int vector[N] = {1, 2, 3, 4};
-*/
