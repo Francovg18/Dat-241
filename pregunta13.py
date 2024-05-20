e5d969654db257e20ff59ab4a377671b0787933e
Francovg18 <153469619+Francovg18@users.noreply.github.com>
2024-05-19 23:35:35 -0400

    Add files via upload

---
+++ pregunta13.py
@@ -0,0 +1,46 @@
+import multiprocessing
+from math import sqrt
+
+def fibonacci(n):
+    """
+    Función para calcular el n-ésimo término de la secuencia de Fibonacci
+    utilizando la fórmula an = (1/sqrt(5)) * ((1+sqrt(5))/2)^n - (1/sqrt(5)) * ((1-sqrt(5))/2)^n
+    """
+    sqrt5 = sqrt(5)
+    phi = (1 + sqrt5) / 2
+    psi = (1 - sqrt5) / 2
+    return int((1 / sqrt5) * (phi ** n - psi ** n))
+
+def trabajador(inicio, fin, cola_resultados):
+    resultados = []
+    for i in range(inicio, fin):
+        resultados.append(fibonacci(i))
+    cola_resultados.put(resultados)
+
+def principal():
+    n = 1000
+    num_procesadores = multiprocessing.cpu_count()
+    tamaño_chunk = n // num_procesadores
+
+    cola_resultados = multiprocessing.Queue()
+
+    procesos = []
+    for i in range(num_procesadores):
+        inicio = i * tamaño_chunk + 1
+        fin = (i + 1) * tamaño_chunk + 1 if i < num_procesadores - 1 else n + 1
+        proceso = multiprocessing.Process(target=trabajador, args=(inicio, fin, cola_resultados))
+        procesos.append(proceso)
+        proceso.start()
+
+    for proceso in procesos:
+        proceso.join()
+
+    # Recolectar resultados de la cola
+    resultados = []
+    while not cola_resultados.empty():
+        resultados.extend(cola_resultados.get())
+
+    print("Secuencia de Fibonacci hasta el término", n, ":", resultados)
+
+if __name__ == "__main__":
+    principal()
