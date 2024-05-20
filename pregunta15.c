ad49f0eb791957e655e60fd678cb66c3bcffb581
Francovg18 <153469619+Francovg18@users.noreply.github.com>
2024-05-20 01:11:07 -0400

    Update pregunta15.c

--- pregunta15.c
+++ pregunta15.c
@@ -6,7 +6,6 @@ int main() {
     #pragma omp parallel
     {
         num_threads = omp_get_num_threads();
-        //printf("Hola Mundo desde el hilo %d de %d\n", omp_get_thread_num(), num_threads);
         printf("hola mundo\n");
     }
     return 0;
